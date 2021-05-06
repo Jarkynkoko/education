5c92c2c97824d58e83edbb8189ebfb9f254ceaf3
alima <kokoyeva@gmail.com>
2021-05-06 15:31:32 +0600

    set up

---
+++ selfPractice/practice.java
@@ -0,0 +1,8 @@
+package selfPractice;
+
+public class practice {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+        System.out.println("java");
+    }
+}
